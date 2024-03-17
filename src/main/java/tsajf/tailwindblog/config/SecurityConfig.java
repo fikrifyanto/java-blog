13cc574f3ad47ef7dd9f6fcadcc90737c6ae15a7
fikrifyanto <fikrifyanto@gmail.com>
2024-03-17 15:19:34 +0700

    Add security config

---
+++ src/main/java/tsajf/tailwindblog/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package tsajf.tailwindblog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(request -> request.requestMatchers("/", "/assets/css/*", "/assets/js/*").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(form -> form.loginPage("/login").permitAll())
+                .logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
+}
