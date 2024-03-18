c23282d8bfe77655c655cb237f57c533dd88434c
fikrifyanto <fikrifyanto@gmail.com>
2024-03-19 00:44:07 +0700

    Add login

---
+++ src/main/java/tsajf/tailwindblog/model/UserModel.java
@@ -0,0 +1,16 @@
+package tsajf.tailwindblog.model;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+
+    private String name;
+
+    private String username;
+
+    private String password;
+
+    private String role;
+
+}
---
+++ src/main/java/tsajf/tailwindblog/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package tsajf.tailwindblog.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import tsajf.tailwindblog.model.UserModel;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel byLogin = userService.findByLogin(username);
+        if(byLogin == null) {
+            return null;
+        }
+        return User.builder()
+                .username(byLogin.getUsername())
+                .password(byLogin.getPassword())
+                .roles(byLogin.getRole())
+                .build();
+    }
+
+}
