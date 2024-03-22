9a7996fe4453e5f36ff38bc09530334de7f74217
fikrifyanto <fikrifyanto@gmail.com>
2024-03-23 00:07:31 +0700

    Fix sidebar

--- src/main/resources/static/assets/js/sidebar.js
+++ src/main/resources/static/assets/js/sidebar.js
@@ -1,16 +1,19 @@
 function bindSidebar() {
     const sidebar = document.getElementById('sidebar')
     const container = document.getElementById('container')
+
     if (sidebar) {
         /** Mobile **/
         if (sidebar.classList.contains('-translate-x-full')) {
             // Open Sidebar
             sidebar.classList.remove('-translate-x-full')
             sidebar.classList.add('translate-x-0')
+            localStorage.setItem('sidebarOpen', 'true');
         } else {
             // Close Sidebar
             sidebar.classList.add('-translate-x-full')
             sidebar.classList.remove('translate-x-0')
+            localStorage.setItem('sidebarOpen', 'false');
         }
 
         /** Desktop **/
@@ -19,14 +22,21 @@ function bindSidebar() {
             sidebar.classList.add('lg:-translate-x-full')
             sidebar.classList.remove('lg:translate-x-0')
             container.classList.add('lg:-ml-[25%]')
+            localStorage.setItem('sidebarOpen', 'true');
         } else {
             // Close Sidebar
             sidebar.classList.remove('lg:-translate-x-full')
             sidebar.classList.add('lg:translate-x-0')
             container.classList.remove('lg:-ml-[25%]')
+            localStorage.setItem('sidebarOpen', 'false');
         }
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sidebarOpen = localStorage.getItem('sidebarOpen');
+    if (sidebarOpen === 'true') bindSidebar();
+});
 
 const openSidebarButton = document.getElementById('open-sidebar-button')
 if (openSidebarButton) {
