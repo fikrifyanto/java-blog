d1418f527a902a8fa971a8af1d97151aa910680e
fikrifyanto <fikrifyanto@gmail.com>
2024-03-23 17:43:32 +0700

    Fix layout and validation media

--- src/main/java/tsajf/tailwindblog/controller/MediaController.java
+++ src/main/java/tsajf/tailwindblog/controller/MediaController.java
@@ -1,10 +1,13 @@
 package tsajf.tailwindblog.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import tsajf.tailwindblog.entity.Media;
 import tsajf.tailwindblog.entity.User;
 import tsajf.tailwindblog.repository.MediaRepository;
@@ -29,32 +32,52 @@ public class MediaController {
 
     @GetMapping("/admin/media")
     public String index(Model model) {
-        model.addAttribute("title", "Media List");
-        model.addAttribute("page", "admin/media/index");
         model.addAttribute("medias", mediaRepository.findAll());
-        return "admin/fragments/layout";
+        return "admin/media/index";
     }
 
     @GetMapping("/admin/media/create")
     public String create(Model model) {
-        model.addAttribute("title", "Add Media");
-        model.addAttribute("page", "admin/media/create");
         model.addAttribute("media", new Media());
-        return "admin/fragments/layout";
+        return "admin/media/create";
+    }
+
+    @PostMapping(value = "/admin/media/store", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String store(
+            @RequestParam("file") MultipartFile file,
+            @ModelAttribute @Valid Media store,
+            BindingResult result,
+            Model model
+    ) throws IOException {
+        if (result.hasErrors()) {
+            model.addAttribute("media", store);
+            return "admin/category/create";
+        }
+
+        mediaService.save(store.getName(), file);
+        return "redirect:/admin/media";
     }
 
     @GetMapping("/admin/media/edit/{id}")
     public String edit(@PathVariable("id") Integer id, Model model) {
         Media media = mediaRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-        model.addAttribute("title", "Edit Media");
-        model.addAttribute("page", "admin/media/edit");
         model.addAttribute("media", media);
-        return "admin/fragments/layout";
+        return "admin/media/edit";
     }
 
     @PostMapping("/admin/media/update/{id}")
-    public String update(@PathVariable("id") Integer id, @ModelAttribute User update) {
+    public String update(
+            @PathVariable("id") Integer id,
+            @ModelAttribute @Valid Media update,
+            BindingResult result,
+            Model model
+    ) {
+        if (result.hasErrors()) {
+            model.addAttribute("media", update);
+            return "admin/media/edit";
+        }
+
         Media media = mediaRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
         media.setName(update.getName());
@@ -62,33 +85,25 @@ public class MediaController {
         return "redirect:/admin/media";
     }
 
-    @PostMapping(value = "/admin/media/store", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public String store(@RequestParam("file") MultipartFile file, @ModelAttribute("uploadForm") Media store) throws IOException {
-        mediaService.save(store.getName(), file);
-        return "redirect:/admin/media";
-    }
-
     @GetMapping("/admin/media/delete/{id}")
-    public String delete(@PathVariable("id") Integer id) {
+    public String delete(@PathVariable("id") Integer id, RedirectAttributes redirectAttributes) {
         Optional<Media> media = mediaRepository.findById(id);
 
-        if(media.isPresent()) {
+        if (media.isPresent()) {
             Path filepath = Path.of(media.get().getPath());
             File file = new File(String.valueOf(filepath));
-            System.out.println(file);
+
             if (file.exists()) {
-                if (file.delete()) {
-                    System.out.println("File deleted successfully.");
-                } else {
-                    System.out.println("Failed to delete the file.");
+                boolean deleted = file.delete();
+                if(!deleted) {
+                    redirectAttributes.addFlashAttribute("error", "File is not exists in storage!");
                 }
-            } else {
-                System.out.println("File not found.");
             }
+
+            mediaRepository.deleteById(id);
+            return "redirect:/admin/media";
         }
 
-        mediaRepository.deleteById(id);
-        return "redirect:/admin/media";
+        return null;
     }
-
 }
