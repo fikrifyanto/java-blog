3cdc37cbc11513ab5cea1b09a79214577897ada6
fikrifyanto <fikrifyanto@gmail.com>
2024-03-24 05:47:23 +0700

    Looping categories and posts

--- src/main/java/tsajf/tailwindblog/controller/VisitorController.java
+++ src/main/java/tsajf/tailwindblog/controller/VisitorController.java
@@ -3,14 +3,36 @@ package tsajf.tailwindblog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import tsajf.tailwindblog.model.Category;
+import tsajf.tailwindblog.model.Post;
+import tsajf.tailwindblog.repository.CategoryRepository;
+import tsajf.tailwindblog.repository.PostRepository;
+import tsajf.tailwindblog.repository.UserRepository;
+
+import java.util.List;
 
 @Controller
 public class VisitorController {
+
+    private final CategoryRepository categoryRepository;
+
+    private final PostRepository postRepository;
+
+    public VisitorController(CategoryRepository categoryRepository, PostRepository postRepository) {
+        this.categoryRepository = categoryRepository;
+        this.postRepository = postRepository;
+    }
+
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("title", "Home");
-        model.addAttribute("page", "visitor/index");
-        return "visitor/fragments/layout";
+
+        List<Category> categories = categoryRepository.findFirst4ByOrderByName();
+        List<Post> posts = postRepository.findAll();
+
+        model.addAttribute("categories", categories);
+        model.addAttribute("posts", posts);
+
+        return "visitor/index";
     }
 
 }
--- src/main/java/tsajf/tailwindblog/repository/CategoryRepository.java
+++ src/main/java/tsajf/tailwindblog/repository/CategoryRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tsajf.tailwindblog.model.Category;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
+    List<Category> findFirst4ByOrderByName();
 
 }
